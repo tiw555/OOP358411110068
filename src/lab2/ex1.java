70dfdd9dca43c79129e48764b497108e8b9335b8
Teera <Teera333555@gmail.com>
2018-11-26 14:34:23 +0700

    lad 2 : Variables

---
+++ src/lab2/ex1.java
@@ -0,0 +1,36 @@
+package lab2;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+    //integer
+    int a;
+    a=10;
+    System.out.println(a);
+int b = 20;
+System.out.println(b);
+int c;
+c=a+b;System.out.println("ผลรวมของ a และ b เท่ากับ \t"+c);
+
+//real number
+        double t;
+        t=5.5;
+System.out.println(t);
+double n = 5.5;
+n = n*10;
+System.out.println(n);
+//character
+        char d = '#';
+        System.out.println(d);
+
+        //String
+        String str = "Teera Katetong";
+System.out.println("my name is "+str);
+System.out.println(str.length());
+System.out.println(str.toUpperCase());
+
+    }//main
+
+
+
+}//class
