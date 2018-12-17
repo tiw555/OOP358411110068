ae683601e46d64c63383332c1089f447bacebd2a
Teera <Teera333555@gmail.com>
2018-12-17 15:13:16 +0700

    lab4 :Methods

---
+++ src/Lab4/ex3.java
@@ -0,0 +1,18 @@
+package Lab4;
+
+public class ex3 {
+
+    public static void main(String[] args) {
+        A(5.5,5);
+    }
+    public static int  A (int a,int b){
+        return a * b;
+    }
+    public static double A (double a,double b){
+       return a * b;
+    }
+
+
+
+
+}//class
