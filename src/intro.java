dfb44f1f591e3b79f67ae68274d48bbe575f4027
tiw555 <45161931+tiw555@users.noreply.github.com>
2019-03-12 13:10:36 +0700

    Add files via upload

---
+++ src/intro.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class intro {
+    public static void main(String[] args){
+
+        System.out.println("TEERA");
+        System.out.println("TEERA333555@gmail.com");
+        System.out.println("information system");
+        System.out.println("RMUSV");
+
+
+
+
+    }//main
+
+
+
+}//class
